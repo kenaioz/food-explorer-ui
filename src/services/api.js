2233e8ba68768300c45293ac737dd5ddeb1111ad
Silvio Cesar <silvio.cesarrx@hotmail.com>
2024-01-16 18:02:49 -0300

    new api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://food-explorer-api-ihgd.onrender.com",
 });
 
 const createSession = (token) => {
