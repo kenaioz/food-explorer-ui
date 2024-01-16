891e1d58ee32649e8f8e8bca804d6eabd61a8869
Silvio Cesar <silvio.cesarrx@hotmail.com>
2024-01-16 20:12:22 -0300

    404 page not found responsive + _redirect file

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import { Container } from "./styles";
 
+import { Layout } from "../../components/Layout";
+
 export function NotFound() {
   return (
     <Container>
-      <h1>404</h1>
-      <h2>Página não encontrada</h2>
-      <Link to="/">Voltar para o início</Link>
+      <Layout>
+        <h1>404</h1>
+        <h2>Página não encontrada</h2>
+        <Link to="/">Voltar para o início</Link>
+      </Layout>
     </Container>
   );
 }
--- src/pages/NotFound/styles.js
+++ src/pages/NotFound/styles.js
@@ -1,27 +1,36 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.div`
-  height: 100vh;
-  width: 100%;
+  div {
+    margin-top: 150px;
+    font-family: Poppins;
 
-  font-family: Poppins;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+    h1 {
+      font-size: 8rem;
+    }
 
-  h1 {
-    font-size: 8rem;
-  }
+    h2 {
+      font-size: 2rem;
 
-  h2 {
-    font-size: 2rem;
-  }
+      @media (max-width: ${DEVICE_BREAKPOINTS.SM}) {
+        font-size: 1rem;
+      }
+    }
 
-  > a {
-    color: ${({ theme }) => theme.COLORS.LIGHT_100};
-    margin-top: 24px;
-    text-decoration: underline;
+    > a {
+      color: ${({ theme }) => theme.COLORS.LIGHT_100};
+      margin-top: 24px;
+      text-decoration: underline;
+
+      @media (max-width: ${DEVICE_BREAKPOINTS.SM}) {
+        font-size: 0.75rem;
+      }
+    }
   }
 `;
